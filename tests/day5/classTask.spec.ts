963a27fd88c7db6a76beaddae1aa7c5da5f05d19
Vignesh Manikandan <vigneshmk.2258@gmail.com>
2026-08-01 12:23:33 +0530

    Day 5 class task completed

---
+++ tests/day5/classTask.spec.ts
@@ -0,0 +1,46 @@
+//Tasks to perform:
+// 1. Navigate to the Salesforce login page.
+// 2. Use different CSS selector strategies (ID, Class, and Attribute) to locate the username 
+// and password fields.
+// 3. Use XPath selectors (Absolute, Relative, and Attribute-based) to also locate the username 
+// and password fields.
+// 4. Enter the sample credentials into the username and password fields.
+// 5. Click the login button using both CSS and XPath selectors.
+// 6. Verify that the login was successful by checking for a specific element on the landing page.
+
+import { test, expect } from '@playwright/test';
+
+test('Testing with selectors', async ({ browser }) => 
+    {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+
+        const testURL = "https://login.salesforce.com/?locale=in";
+        const username = "dilipkumar.rajendran@testleaf.com";
+        const password = "TestLeaf@2025";
+
+        await page.goto(testURL);
+
+        // Detect if the page opening is a login page.
+        await expect(page.getByRole('heading', { name: 'Salesforce Login' })).toBeVisible();
+        
+        // Using CSS Selectors for locating username and password fields
+        await page.locator('#username').fill(username); 
+        await page.locator('#password').fill(password);
+
+        // Using XPath Selectors for locating username and password fields
+        await page.locator('//*[@id="username"]').fill(username);// XPath: //*[@id="username"]
+        await page.locator('//*[@id="password"]').fill(password);// XPath: //*[@id="password"]
+
+        // Click the login button using CSS Selector
+        await page.locator('#Login'); // #Login
+        // Click the login button using XPath Selector
+        await page.locator('//*[@id="Login"]').click(); //*[@id="Login"]
+
+        await page.waitForTimeout(10000); // 1 second = 1000 milliseconds
+
+        // Checking for a specific element on the landing page to verify successful login.
+        await expect(page.getByRole('heading', { name: 'Marketing CRM Classic' })).toBeVisible();
+
+        browser.close();
+    });
