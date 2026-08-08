c331574f5a389f2786fc8a76908b7b8d97afe3d3
Vignesh Manikandan <vigneshmk.2258@gmail.com>
2026-08-08 10:08:24 +0530

    Day 6 Assignments completed

--- tests/day6/task3.spec.ts
+++ tests/day6/task3.spec.ts
@@ -24,6 +24,9 @@ test('Assignment 3 (Locators & Assertions)', async ({ browser }) =>
         const username = "dilipkumar.rajendran@testleaf.com";
         const password = "TestLeaf@2025";
 
+        // New Credentials
+        const AccountName = "John Doe";
+
         // 1. Navigate to the url https://login.salesforce.com/ 
         await page.goto(testURL);
 
@@ -55,11 +58,40 @@ test('Assignment 3 (Locators & Assertions)', async ({ browser }) =>
         await page.getByRole('dialog', { name: "App Launcher"}).getByPlaceholder('Search apps or items...').fill('Service');
 
         // 9. Click Service using index based XPath 
-        await expect(page.getByRole('dialog', { name: "App Launcher"}).locator('(p[text()="Service"])[1]')).toBeVisible();
+        await expect(page.getByRole('dialog', { name: "App Launcher"}).locator('//*[@class="slds-truncate"]').first()).toBeVisible();
 
-        await page.getByRole('dialog', { name: "App Launcher"}).locator('(p[text()="Service"])[1]');
+        const servicesTab = page.getByRole('dialog', { name: "App Launcher"}).locator('//*[@class="slds-truncate"]').first();
 
-        await page.waitForTimeout(4000);
+        await servicesTab.click();
+
+        await page.waitForTimeout(2000);
+
+        await expect(page.getByTitle('Service').filter({ hasText: 'Service'})).toBeVisible();
+
+        // 10. Click Accounts using attribute based CSS selector
+        await page.locator('a[title="Accounts"]').click();
+
+        await expect(page.getByRole('heading', { name: 'Accounts', exact: true})).toBeVisible();
+
+        // 11. Click New using getByRole 
+        await page.getByRole('button', { name: 'New', exact: true}).click();
+
+        await expect(page.locator('records-lwc-detail-panel').getByRole('heading', { name: 'New Account'})).toBeVisible();
+
+        const newContainer = page.locator('records-lwc-detail-panel');
+
+        // 12. Enter Account name using attribute based CSS selector 
+        await newContainer.locator('input[name="Name"]').fill(AccountName);
+
+        // 13. Click Save button using XPath
+        await page.locator('//button[@name="SaveEdit"]').click();
+
+        // 14. Verify the toast message displayed
+        const toastMsg = `Account "${AccountName}" was created.`;
+
+        await expect(page.locator('//*[@class="toastMessage slds-text-heading--small forceActionsText"]')).toHaveText(toastMsg);
+
+        await page.waitForTimeout(1000);
 
         await browser.close();
     });
